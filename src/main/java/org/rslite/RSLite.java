f6748e9d193397e5d73410352793954712a55daf
Shed- <ahmadcoolboy74@hotmail.com>
2014-07-19 20:39:28 +0300

    Collapsed catch block
    usage of | for all exceptions since they all have the same code.

--- src/main/java/org/rslite/RSLite.java
+++ src/main/java/org/rslite/RSLite.java
@@ -22,16 +22,9 @@ public class RSLite {
 					break;
 				}
 			}
-		} catch (ClassNotFoundException ex) {
-			java.util.logging.Logger.getLogger(GameSelect.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-		} catch (InstantiationException ex) {
-			java.util.logging.Logger.getLogger(GameSelect.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-		} catch (IllegalAccessException ex) {
-			java.util.logging.Logger.getLogger(GameSelect.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-		} catch (javax.swing.UnsupportedLookAndFeelException ex) {
-			java.util.logging.Logger.getLogger(GameSelect.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-		}
-
+    } catch (ClassNotFoundException | InstantiationException | IllegalAccessException | UnsupportedLookAndFeelException ex) {
+        java.util.logging.Logger.getLogger(GameSelect.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+    }
 		java.awt.EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				new GameSelect().setVisible(true);
